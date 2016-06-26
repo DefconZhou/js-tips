711261c292e6254db16342f404d99749b79e5468
DefconZhou <smtppop3@outlook.com>
2016-06-26 15:54:14 +0800

    add custom exception example

---
+++ exception/CustomException.js
@@ -0,0 +1,12 @@
+// @author defcon
+// @Ref https://www.sitepoint.com/exceptional-exception-handling-in-javascript/
+
+function LocalStorageException(message) {
+	this.name = "LocalStorageException";
+	this.message = message;
+}
+
+LocalStorageException.prototype = new Error();
+
+LocalStorageException.prototype.constructor = LocalStorageException;
+
